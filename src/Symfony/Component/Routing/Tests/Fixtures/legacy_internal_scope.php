0aaeef6bfe6492853cb5540dd68a8d796e69af59
Nicolas Grekas <nicolas.grekas@gmail.com>
2025-08-21 14:47:46 +0200

    [DependencyInjection][Routing] Handle declaring services and routes using PHP arrays that follow the same shape as corresponding yaml files

--- src/Symfony/Component/Routing/Tests/Fixtures/legacy_internal_scope.php
+++ src/Symfony/Component/Routing/Tests/Fixtures/legacy_internal_scope.php
@@ -3,6 +3,6 @@
 use Symfony\Component\Routing\RouteCollection;
 
 // access the loader's internal scope to trigger deprecation
-$loader->callConfigurator(static fn () => 'dummy', 'dummy.php', 'dummy.php');
+$loader->callConfigurator(static fn () => [], 'dummy.php', 'dummy.php');
 
 return new RouteCollection();
