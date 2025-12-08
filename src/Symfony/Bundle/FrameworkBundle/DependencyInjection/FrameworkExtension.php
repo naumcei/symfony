naumcei/symfony

commit ea5a3ccdf66474aaa06bb95f38fc56b8e9589ec6

Remove ClassMetadataFactoryCompiler and other legacy cleanups

src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
@@ -43,8 +43,6 @@ use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\Cache\Adapter\ChainAdapter;
 use Symfony\Component\Cache\Adapter\TagAwareAdapter;
 use Symfony\Component\Cache\DependencyInjection\CachePoolPass;
-use Symfony\Component\Cache\Marshaller\MarshallerInterface;
-use Symfony\Component\Cache\ResettableInterface;
 use Symfony\Component\Clock\ClockInterface;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\FileLocator;
@@ -55,11 +53,8 @@ use Symfony\Component\Config\ResourceCheckerInterface;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\DataCollector\CommandDataCollector;
-use Symfony\Component\Console\Debug\CliRequest;
 use Symfony\Component\Console\Messenger\RunCommandMessageHandler;
 use Symfony\Component\DependencyInjection\Alias;
-use Symfony\Component\DependencyInjection\Argument\ArgumentTrait;
 use Symfony\Component\DependencyInjection\Argument\IteratorArgument;
 use Symfony\Component\DependencyInjection\Argument\ServiceLocatorArgument;
 use Symfony\Component\DependencyInjection\ChildDefinition;
@@ -83,7 +78,6 @@ use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\Glob;
-use Symfony\Component\Form\EnumFormTypeGuesser;
 use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormTypeExtensionInterface;
 use Symfony\Component\Form\FormTypeGuesserInterface;
@@ -106,7 +100,6 @@ use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
 use Symfony\Component\HttpKernel\DataCollector\DataCollectorInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\HttpKernel\Log\DebugLoggerConfigurator;
-use Symfony\Component\HttpKernel\Profiler\ProfilerStateChecker;
 use Symfony\Component\JsonStreamer\Attribute\JsonStreamable;
 use Symfony\Component\JsonStreamer\JsonStreamWriter;
 use Symfony\Component\JsonStreamer\StreamReaderInterface;
@@ -123,11 +116,9 @@ use Symfony\Component\Mercure\HubRegistry;
 use Symfony\Component\Messenger\Attribute\AsMessage;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Messenger\Bridge as MessengerBridge;
-use Symfony\Component\Messenger\EventListener\ResetMemoryUsageListener;
 use Symfony\Component\Messenger\Handler\BatchHandlerInterface;
 use Symfony\Component\Messenger\MessageBus;
 use Symfony\Component\Messenger\MessageBusInterface;
-use Symfony\Component\Messenger\Middleware\DeduplicateMiddleware;
 use Symfony\Component\Messenger\Middleware\RouterContextMiddleware;
 use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
 use Symfony\Component\Messenger\Transport\TransportFactoryInterface as MessengerTransportFactoryInterface;
@@ -164,12 +155,10 @@ use Symfony\Component\RateLimiter\Storage\CacheStorage;
 use Symfony\Component\RemoteEvent\Attribute\AsRemoteEventConsumer;
 use Symfony\Component\RemoteEvent\RemoteEvent;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Routing\Loader\AttributeServicesLoader;
 use Symfony\Component\Scheduler\Attribute\AsCronTask;
 use Symfony\Component\Scheduler\Attribute\AsPeriodicTask;
 use Symfony\Component\Scheduler\Attribute\AsSchedule;
 use Symfony\Component\Scheduler\Messenger\SchedulerTransportFactory;
-use Symfony\Component\Scheduler\Messenger\Serializer\Normalizer\SchedulerTriggerNormalizer;
 use Symfony\Component\Security\Core\AuthenticationEvents;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Csrf\CsrfToken;
@@ -179,7 +168,6 @@ use Symfony\Component\Semaphore\Semaphore;
 use Symfony\Component\Semaphore\SemaphoreFactory;
 use Symfony\Component\Semaphore\Store\StoreFactory as SemaphoreStoreFactory;
 use Symfony\Component\Serializer\Attribute as SerializerMapping;
-use Symfony\Component\Serializer\DependencyInjection\AttributeMetadataPass as SerializerAttributeMetadataPass;
 use Symfony\Component\Serializer\Encoder\DecoderInterface;
 use Symfony\Component\Serializer\Encoder\EncoderInterface;
 use Symfony\Component\Serializer\Mapping\Loader\XmlFileLoader;
@@ -205,9 +193,7 @@ use Symfony\Component\Uid\Factory\UuidFactory;
 use Symfony\Component\Uid\UuidV4;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Constraints\ExpressionLanguageProvider;
-use Symfony\Component\Validator\Constraints\Traverse;
 use Symfony\Component\Validator\ConstraintValidatorInterface;
-use Symfony\Component\Validator\DependencyInjection\AttributeMetadataPass as ValidatorAttributeMetadataPass;
 use Symfony\Component\Validator\GroupProviderInterface;
 use Symfony\Component\Validator\Mapping\Loader\PropertyInfoLoader;
 use Symfony\Component\Validator\ObjectInitializerInterface;
@@ -676,12 +662,6 @@ class FrameworkExtension extends Extension
             ->addTag('kernel.locale_aware');
         $container->registerForAutoconfiguration(ResetInterface::class)
             ->addTag('kernel.reset', ['method' => 'reset']);
-
-        if (!interface_exists(MarshallerInterface::class)) {
-            $container->registerForAutoconfiguration(ResettableInterface::class)
-                ->addTag('kernel.reset', ['method' => 'reset']);
-        }
-
         $container->registerForAutoconfiguration(PropertyListExtractorInterface::class)
             ->addTag('property_info.list_extractor');
         $container->registerForAutoconfiguration(PropertyTypeExtractorInterface::class)
@@ -843,10 +823,6 @@ class FrameworkExtension extends Extension
     {
         $loader->load('form.php');
 
-        if (!class_exists(EnumFormTypeGuesser::class)) {
-            $container->removeDefinition('form.type_guesser.enum_type');
-        }
-
         if (null === $config['form']['csrf_protection']['enabled']) {
             $this->writeConfigEnabled('form.csrf_protection', $config['csrf_protection']['enabled'], $config['form']['csrf_protection']);
         }
@@ -945,11 +921,6 @@ class FrameworkExtension extends Extension
         $loader->load('collectors.php');
         $loader->load('cache_debug.php');
 
-        if (!class_exists(ProfilerStateChecker::class)) {
-            $container->removeDefinition('profiler.state_checker');
-            $container->removeDefinition('profiler.is_disabled_state_checker');
-        }
-
         if ($this->isInitializedConfigEnabled('form')) {
             $loader->load('form_debug.php');
         }
@@ -1006,12 +977,8 @@ class FrameworkExtension extends Extension
         $container->getDefinition('profiler_listener')
             ->addArgument($config['collect_parameter']);
 
-        if (!$container->getParameter('kernel.debug') || !class_exists(CliRequest::class) || !$container->has('debug.stopwatch')) {
+        if (!$container->getParameter('kernel.debug') || !$container->has('debug.stopwatch')) {
             $container->removeDefinition('console_profiler_listener');
-        }
-
-        if (!class_exists(CommandDataCollector::class)) {
-            $container->removeDefinition('.data_collector.command');
         }
     }
 
@@ -1269,10 +1236,6 @@ class FrameworkExtension extends Extension
         }
 
         $loader->load('routing.php');
-
-        if (!class_exists(AttributeServicesLoader::class)) {
-            $container->removeDefinition('routing.loader.attribute.services');
-        }
 
         if ($config['utf8']) {
             $container->getDefinition('routing.loader')->replaceArgument(1, ['utf8' => true]);
@@ -1766,7 +1729,7 @@ class FrameworkExtension extends Extension
 
         // When attributes are disabled, it means from runtime-discovery only; autoconfiguration should still happen.
         // And when runtime-discovery of attributes is enabled, we can skip compile-time autoconfiguration in debug mode.
-        if (class_exists(ValidatorAttributeMetadataPass::class) && (!($config['enable_attributes'] ?? false) || !$container->getParameter('kernel.debug')) && trait_exists(ArgumentTrait::class)) {
+        if (!($config['enable_attributes'] ?? false) || !$container->getParameter('kernel.debug')) {
             // The $reflector argument hints at where the attribute could be used
             $container->registerAttributeForAutoconfiguration(Constraint::class, function (ChildDefinition $definition, Constraint $attribute, \ReflectionClass|\ReflectionMethod|\ReflectionProperty $reflector) {
                 $definition->addTag('validator.attribute_metadata');
@@ -1818,9 +1781,6 @@ class FrameworkExtension extends Extension
 
         if (!ContainerBuilder::willBeAvailable('symfony/form', Form::class, ['symfony/framework-bundle', 'symfony/validator'])) {
             $container->removeDefinition('validator.form.attribute_metadata');
-        } elseif (!($r = new \ReflectionClass(Form::class))->getAttributes(Traverse::class) || !class_exists(ValidatorAttributeMetadataPass::class)) {
-            // BC with symfony/form & symfony/validator < 7.4
-            $fileRecorder('xml', \dirname($r->getFileName()).'/Resources/config/validation.xml');
         }
 
         foreach ($container->getParameter('kernel.bundles_metadata') as $bundle) {
@@ -2016,7 +1976,7 @@ class FrameworkExtension extends Extension
 
         // When attributes are disabled, it means from runtime-discovery only; autoconfiguration should still happen.
         // And when runtime-discovery of attributes is enabled, we can skip compile-time autoconfiguration in debug mode.
-        if (class_exists(SerializerAttributeMetadataPass::class) && (!($config['enable_attributes'] ?? false) || !$container->getParameter('kernel.debug'))) {
+        if (!($config['enable_attributes'] ?? false) || !$container->getParameter('kernel.debug')) {
             // The $reflector argument hints at where the attribute could be used
             $configurator = function (ChildDefinition $definition, object $attribute, \ReflectionClass|\ReflectionMethod|\ReflectionProperty $reflector) {
                 $definition->addTag('serializer.attribute_metadata');
@@ -2037,15 +1997,10 @@ class FrameworkExtension extends Extension
             });
         }
 
-        if (($config['enable_attributes'] ?? false) || class_exists(SerializerAttributeMetadataPass::class)) {
-            $serializerLoaders[] = new Reference('serializer.mapping.attribute_loader');
+        $serializerLoaders[] = new Reference('serializer.mapping.attribute_loader');
 
-            $container->getDefinition('serializer.mapping.attribute_loader')
-                ->replaceArgument(0, $config['enable_attributes'] ?? false);
-        } else {
-            // BC with symfony/serializer < 7.4
-            $container->removeDefinition('serializer.mapping.attribute_services_loader');
-        }
+        $container->getDefinition('serializer.mapping.attribute_loader')
+            ->replaceArgument(0, $config['enable_attributes'] ?? false);
 
         $fileRecorder = function ($extension, $path) use (&$serializerLoaders) {
             $definition = new Definition(\in_array($extension, ['yaml', 'yml'], true) ? YamlFileLoader::class : XmlFileLoader::class, [$path]);
@@ -2289,11 +2244,6 @@ class FrameworkExtension extends Extension
         if (!$this->hasConsole()) {
             $container->removeDefinition('console.command.scheduler_debug');
         }
-
-        // BC layer Scheduler < 7.3
-        if (!ContainerBuilder::willBeAvailable('symfony/serializer', DenormalizerInterface::class, ['symfony/framework-bundle', 'symfony/scheduler']) || !class_exists(SchedulerTriggerNormalizer::class)) {
-            $container->removeDefinition('serializer.normalizer.scheduler_trigger');
-        }
     }
 
     private function registerMessengerConfiguration(array $config, ContainerBuilder $container, PhpFileLoader $loader, bool $validationEnabled, bool $lockEnabled): void
@@ -2310,10 +2260,6 @@ class FrameworkExtension extends Extension
 
         if (!interface_exists(DenormalizerInterface::class)) {
             $container->removeDefinition('serializer.normalizer.flatten_exception');
-        }
-
-        if (!class_exists(ResetMemoryUsageListener::class)) {
-            $container->removeDefinition('messenger.listener.reset_memory_usage');
         }
 
         if (ContainerBuilder::willBeAvailable('symfony/amqp-messenger', MessengerBridge\Amqp\Transport\AmqpTransportFactory::class, ['symfony/framework-bundle', 'symfony/messenger'])) {
@@ -2356,7 +2302,7 @@ class FrameworkExtension extends Extension
             ],
         ];
 
-        if ($lockEnabled && class_exists(DeduplicateMiddleware::class) && class_exists(LockFactory::class)) {
+        if ($lockEnabled && class_exists(LockFactory::class)) {
             $defaultMiddleware['before'][] = ['id' => 'deduplicate_middleware'];
         } else {
             $container->removeDefinition('messenger.middleware.deduplicate_middleware');

src/Symfony/Component/Form/Resources/config/validation.xml
@@ -1,13 +1,0 @@
-<?xml version="1.0" ?>
-
-<constraint-mapping xmlns="http://symfony.com/schema/dic/constraint-mapping"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://symfony.com/schema/dic/constraint-mapping https://symfony.com/schema/dic/constraint-mapping/constraint-mapping-1.0.xsd">
-
-  <class name="Symfony\Component\Form\Form">
-    <constraint name="Symfony\Component\Form\Extension\Validator\Constraints\Form" />
-    <constraint name="Symfony\Component\Validator\Constraints\Traverse">
-      <option name="traverse">false</option>
-    </constraint>
-  </class>
-</constraint-mapping>

src/Symfony/Component/HttpKernel/EventListener/DebugHandlersListener.php
@@ -11,7 +11,6 @@
 
 namespace Symfony\Component\HttpKernel\EventListener;
 
-use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\ConsoleEvents;
 use Symfony\Component\Console\Event\ConsoleEvent;
 use Symfony\Component\Console\Output\ConsoleOutputInterface;
@@ -38,16 +37,10 @@ class DebugHandlersListener implements EventSubscriberInterface
     private bool $hasTerminatedWithException = false;
 
     /**
-     * @param bool          $webMode
      * @param callable|null $exceptionHandler A handler that must support \Throwable instances that will be called on Exception
      */
-    public function __construct(?callable $exceptionHandler = null, bool|LoggerInterface|null $webMode = null)
+    public function __construct(?callable $exceptionHandler = null, ?bool $webMode = null)
     {
-        if ($webMode instanceof LoggerInterface) {
-            // BC with Symfony 5
-            $webMode = null;
-        }
-
         $handler = set_exception_handler('var_dump');
         $this->earlyHandler = \is_array($handler) ? $handler[0] : null;
         restore_exception_handler();

src/Symfony/Component/Serializer/Mapping/Factory/ClassMetadataFactoryCompiler.php
@@ -1,73 +1,0 @@
-<?php
-
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-namespace Symfony\Component\Serializer\Mapping\Factory;
-
-use Symfony\Component\Serializer\Mapping\ClassMetadataInterface;
-use Symfony\Component\VarExporter\VarExporter;
-
-trigger_deprecation('symfony/serializer', '7.4', 'The "%s" class is deprecated.', ClassMetadataFactoryCompiler::class);
-
-/**
- * @author Fabien Bourigault <bourigaultfabien@gmail.com>
- *
- * @deprecated since Symfony 7.4
- */
-final class ClassMetadataFactoryCompiler
-{
-    /**
-     * @param ClassMetadataInterface[] $classMetadatas
-     */
-    public function compile(array $classMetadatas): string
-    {
-        return <<<EOF
-            <?php
-
-            // This file has been auto-generated by the Symfony Serializer Component.
-
-            return [{$this->generateDeclaredClassMetadata($classMetadatas)}
-            ];
-            EOF;
-    }
-
-    /**
-     * @param ClassMetadataInterface[] $classMetadatas
-     */
-    private function generateDeclaredClassMetadata(array $classMetadatas): string
-    {
-        $compiled = '';
-
-        foreach ($classMetadatas as $classMetadata) {
-            $attributesMetadata = [];
-            foreach ($classMetadata->getAttributesMetadata() as $attributeMetadata) {
-                $attributesMetadata[$attributeMetadata->getName()] = [
-                    $attributeMetadata->getGroups(),
-                    $attributeMetadata->getMaxDepth(),
-                    $attributeMetadata->getSerializedName(),
-                    $attributeMetadata->getSerializedPath(),
-                ];
-            }
-
-            $classDiscriminatorMapping = $classMetadata->getClassDiscriminatorMapping() ? [
-                $classMetadata->getClassDiscriminatorMapping()->getTypeProperty(),
-                $classMetadata->getClassDiscriminatorMapping()->getTypesMapping(),
-                $classMetadata->getClassDiscriminatorMapping()->getDefaultType(),
-            ] : null;
-
-            $compiled .= \sprintf("\n'%s' => %s,", $classMetadata->getName(), VarExporter::export([
-                $attributesMetadata,
-                $classDiscriminatorMapping,
-            ]));
-        }
-
-        return $compiled;
-    }
-}

src/Symfony/Component/Serializer/Mapping/Loader/AttributeLoader.php
@@ -44,14 +44,12 @@ class AttributeLoader implements LoaderInterface
     ];
 
     /**
-     * @param bool|null      $allowAnyClass Null is allowed for BC with Symfony <= 6
      * @param class-string[] $mappedClasses
      */
     public function __construct(
-        private ?bool $allowAnyClass = true,
+        private bool $allowAnyClass = true,
         private array $mappedClasses = [],
     ) {
-        $this->allowAnyClass ??= true;
     }
 
     /**

src/Symfony/Component/Serializer/Tests/Mapping/Factory/ClassMetadataFactoryCompilerTest.php
@@ -1,136 +1,0 @@
-<?php
-
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-namespace Symfony\Component\Serializer\Tests\Mapping\Factory;
-
-use PHPUnit\Framework\Attributes\Group;
-use PHPUnit\Framework\Attributes\IgnoreDeprecations;
-use PHPUnit\Framework\TestCase;
-use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
-use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactoryCompiler;
-use Symfony\Component\Serializer\Mapping\Loader\AttributeLoader;
-use Symfony\Component\Serializer\Tests\Fixtures\Attributes\AbstractDummy;
-use Symfony\Component\Serializer\Tests\Fixtures\Attributes\AbstractDummyFirstChild;
-use Symfony\Component\Serializer\Tests\Fixtures\Attributes\AbstractDummySecondChild;
-use Symfony\Component\Serializer\Tests\Fixtures\Attributes\AbstractDummyThirdChild;
-use Symfony\Component\Serializer\Tests\Fixtures\Attributes\MaxDepthDummy;
-use Symfony\Component\Serializer\Tests\Fixtures\Attributes\SerializedNameDummy;
-use Symfony\Component\Serializer\Tests\Fixtures\Attributes\SerializedPathDummy;
-use Symfony\Component\Serializer\Tests\Fixtures\Attributes\SerializedPathInConstructorDummy;
-use Symfony\Component\Serializer\Tests\Fixtures\Dummy;
-
-#[IgnoreDeprecations]
-#[Group('legacy')]
-final class ClassMetadataFactoryCompilerTest extends TestCase
-{
-    private string $dumpPath;
-
-    protected function setUp(): void
-    {
-        $this->dumpPath = tempnam(sys_get_temp_dir(), 'sf_serializer_metadata_');
-    }
-
-    protected function tearDown(): void
-    {
-        @unlink($this->dumpPath);
-    }
-
-    public function testItDumpMetadata()
-    {
-        $classMetatadataFactory = new ClassMetadataFactory(new AttributeLoader());
-
-        $dummyMetadata = $classMetatadataFactory->getMetadataFor(Dummy::class);
-        $abstractDummyMetadata = $classMetatadataFactory->getMetadataFor(AbstractDummy::class);
-        $maxDepthDummyMetadata = $classMetatadataFactory->getMetadataFor(MaxDepthDummy::class);
-        $serializedNameDummyMetadata = $classMetatadataFactory->getMetadataFor(SerializedNameDummy::class);
-        $serializedPathDummyMetadata = $classMetatadataFactory->getMetadataFor(SerializedPathDummy::class);
-        $serializedPathInConstructorDummyMetadata = $classMetatadataFactory->getMetadataFor(SerializedPathInConstructorDummy::class);
-
-        $code = (new ClassMetadataFactoryCompiler())->compile([
-            $dummyMetadata,
-            $abstractDummyMetadata,
-            $maxDepthDummyMetadata,
-            $serializedNameDummyMetadata,
-            $serializedPathDummyMetadata,
-            $serializedPathInConstructorDummyMetadata,
-        ]);
-
-        file_put_contents($this->dumpPath, $code);
-        $compiledMetadata = require $this->dumpPath;
-
-        $this->assertCount(6, $compiledMetadata);
-
-        $this->assertArrayHasKey(Dummy::class, $compiledMetadata);
-        $this->assertEquals([
-            [
-                'foo' => [[], null, null, null],
-                'bar' => [[], null, null, null],
-                'baz' => [[], null, null, null],
-                'qux' => [[], null, null, null],
-            ],
-            null,
-        ], $compiledMetadata[Dummy::class]);
-
-        $this->assertArrayHasKey(AbstractDummy::class, $compiledMetadata);
-        $this->assertEquals([
-            [
-                'foo' => [[], null, null, null],
-            ],
-            [
-                'type',
-                [
-                    'first' => AbstractDummyFirstChild::class,
-                    'second' => AbstractDummySecondChild::class,
-                    'third' => AbstractDummyThirdChild::class,
-                ],
-                'third',
-            ],
-        ], $compiledMetadata[AbstractDummy::class]);
-
-        $this->assertArrayHasKey(MaxDepthDummy::class, $compiledMetadata);
-        $this->assertEquals([
-            [
-                'foo' => [[], 2, null, null],
-                'bar' => [[], 3, null, null],
-                'child' => [[], null, null, null],
-            ],
-            null,
-        ], $compiledMetadata[MaxDepthDummy::class]);
-
-        $this->assertArrayHasKey(SerializedNameDummy::class, $compiledMetadata);
-        $this->assertEquals([
-            [
-                'foo' => [[], null, 'baz', null],
-                'bar' => [[], null, 'qux', null],
-                'quux' => [[], null, null, null],
-                'child' => [[], null, null, null],
-            ],
-            null,
-        ], $compiledMetadata[SerializedNameDummy::class]);
-
-        $this->assertArrayHasKey(SerializedPathDummy::class, $compiledMetadata);
-        $this->assertEquals([
-            [
-                'three' => [[], null, null, '[one][two]'],
-                'seven' => [[], null, null, '[three][four]'],
-            ],
-            null,
-        ], $compiledMetadata[SerializedPathDummy::class]);
-
-        $this->assertArrayHasKey(SerializedPathInConstructorDummy::class, $compiledMetadata);
-        $this->assertEquals([
-            [
-                'three' => [[], null, null, '[one][two]'],
-            ],
-            null,
-        ], $compiledMetadata[SerializedPathInConstructorDummy::class]);
-    }
-}

commit 5ab532287e9f06d5df9b168f5c855e8e64487934

only load console profiling if console is actually available
this fixes a regression introduced in ea5a3ccdf66474aaa06bb95f38fc56b8e9589ec6

src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
@@ -646,7 +646,7 @@ class FrameworkExtension extends Extension
             $this->registerNotifierConfiguration($config['notifier'], $container, $loader, $this->readConfigEnabled('webhook', $container, $config['webhook']));
         }
 
-        // profiler depends on form, validation, translation, messenger, mailer, http-client, notifier, serializer being registered
+        // profiler depends on form, validation, translation, messenger, mailer, http-client, notifier, serializer being registered. console is optional
         $this->registerProfilerConfiguration($config['profiler'], $container, $loader);
 
         if ($this->readConfigEnabled('webhook', $container, $config['webhook'])) {
@@ -1078,7 +1078,7 @@ class FrameworkExtension extends Extension
         $container->getDefinition('profiler_listener')
             ->addArgument($config['collect_parameter']);
 
-        if (!$container->getParameter('kernel.debug') || !class_exists(CliRequest::class) || !$container->has('debug.stopwatch')) {
+        if (!$container->getParameter('kernel.debug') || !$this->hasConsole() || !class_exists(CliRequest::class) || !$container->has('debug.stopwatch')) {
             $container->removeDefinition('console_profiler_listener');
         }
 